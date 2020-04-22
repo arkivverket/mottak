cb866d59f89a7338fbd3e684d9412835958ab209
Martin Setek <marset@arkivverket.no>
2020-04-22 14:04:23 +0200

    use TEST_SECRET

--- example/example.py
+++ example/example.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-SECRET = os.getenv("EXAMPLE_SECRET")
+SECRET = os.getenv("TEST_SECRET")
 
 if SECRET is not None:
     print("The secret is: " + SECRET) # Secrets should not be logged!
 else:
-    print("Something is wrong, could not find the env variable EXAMPLE_SECRET", file=sys.stderr)
+    print("Something is wrong, could not find the env variable TEST_SECRET", file=sys.stderr)
