248037979688b99a0bf005073ce121da76d1aeb8
Per Buer <per.buer@gmail.com>
2020-08-12 15:17:38 +0200

    shell quoting bug found

--- tusd/start.sh
+++ tusd/start.sh
@@ -7,17 +7,33 @@ if [ -n "$BASE_PATH" ]; then
     OPT_PARAMS="$OPT_PARAMS --base-path $BASE_PATH"
 fi
 
+echo "Enviroment overview:"
+echo "Bucket: ${BUCKET}"
+echo "Endpoint: ${ENDPOINT}"
+echo "AWS Key: ${AWS_ACCESS_KEY_ID}"
+if [ -n "$AWS_SECRET_ACCESS_KEY" ];
+    then echo "AWS_SECRET_ACCESS_KEY is set (secret)"
+fi
+echo "AWS Region: ${AWS_REGION}"
+echo "Mailgun domain: ${MAILGUN_DOMAIN}"
+if [ -n "$MAILGUN_API_KEY" ];
+    then echo "MAILGUN_API_KEY is set (secret)"
+fi
+echo "Hooks present:"
+find /srv/tusd-hooks
+ 
 # pick GCS:
 if [ "$OBJECTSTORE" == "gcs" ]; then
     echo "Backend is GCS (bucket: $BUCKET). Setting GCS_SERVICE_ACCOUNT_FILE to $AUTH_TOKEN"
     export GCS_SERVICE_ACCOUNT_FILE=$AUTH_TOKEN
-    TUSD_PARAMS="--hooks-dir /srv/tusd-hooks --behind-proxy --gcs-bucket $BUCKET"
+    TUSD_PARAMS="-hooks-dir /srv/tusd-hooks -behind-proxy -gcs-bucket ${BUCKET}"
 # handle Azure here if we're supporting it:
 else
     echo "Assuming we're using the S3 backend"
-    TUSD_PARAMS="--hooks-dir /srv/tusd-hooks --behind-proxy --s3-bucket $BUCKET --s3-endpoint $ENDPOINT"
+    TUSD_PARAMS="-hooks-dir /srv/tusd-hooks -behind-proxy -s3-bucket ${BUCKET} -s3-endpoint ${ENDPOINT}"
 fi
 
 echo tusd command line: "tusd $TUSD_PARAMS $OPT_PARAMS"
-tusd "$TUSD_PARAMS" "$OPT_PARAMS"
+echo "============"
+tusd ${TUSD_PARAMS} ${OPT_PARAMS}
 
