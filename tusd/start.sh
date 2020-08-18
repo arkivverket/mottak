d9a984ad161461411fa68b0a5fc76253a76cc523
Per Buer <per.buer@gmail.com>
2020-08-18 16:05:06 +0200

    Try experimental tusd to see how ABS performs

--- tusd/start.sh
+++ tusd/start.sh
@@ -11,23 +11,29 @@ echo "Enviroment overview:"
 echo "Bucket: ${BUCKET}"
 echo "Endpoint: ${ENDPOINT}"
 echo "AWS Key: ${AWS_ACCESS_KEY_ID}"
-if [ -n "$AWS_SECRET_ACCESS_KEY" ];
-    then echo "AWS_SECRET_ACCESS_KEY is set (secret)"
+if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "AWS_SECRET_ACCESS_KEY is set (secret)"
 fi
 echo "AWS Region: ${AWS_REGION}"
 echo "Mailgun domain: ${MAILGUN_DOMAIN}"
-if [ -n "$MAILGUN_API_KEY" ];
-    then echo "MAILGUN_API_KEY is set (secret)"
+if [ -n "$MAILGUN_API_KEY" ]; then
+    echo "MAILGUN_API_KEY is set (secret)"
 fi
 echo "Hooks present:"
 find /srv/hooks
- 
+
 # pick GCS:
 if [ "$OBJECTSTORE" == "gcs" ]; then
     echo "Backend is GCS (bucket: $BUCKET). Setting GCS_SERVICE_ACCOUNT_FILE to $AUTH_TOKEN"
     export GCS_SERVICE_ACCOUNT_FILE=$AUTH_TOKEN
     TUSD_PARAMS="-hooks-dir /srv/hooks -behind-proxy -gcs-bucket ${BUCKET}"
 # handle Azure here if we're supporting it:
+elif [ "$OBJECTSTORE" == "azure" ]; then
+    echo "Backend is Azure Blob Storage."
+    #  Use Azure BlockBlob Storage with this container name as a storage backend 
+    # (requires the AZURE_ACCOUNT_NAME and AZURE_ACCOUNT_KEY environment variable to be set)
+
+    TUSD_PARAMS="-hooks-dir /srv/hooks -behind-proxy -azure-storage ${BUCKET}"
 else
     echo "Assuming we're using the S3 backend"
     TUSD_PARAMS="-hooks-dir /srv/hooks -behind-proxy -s3-bucket ${BUCKET} -s3-endpoint ${ENDPOINT}"
@@ -36,4 +42,3 @@ fi
 echo tusd command line: "tusd $TUSD_PARAMS $OPT_PARAMS"
 echo "============"
 tusd ${TUSD_PARAMS} ${OPT_PARAMS}
-
