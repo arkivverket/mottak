7bba1d724973c6ee7f858b612d33862401f41b53
Per Buer <per.buer@gmail.com>
2020-08-25 14:31:35 +0200

    Handle peer reset, have a default on size (set via env)

--- s3-scan-tar/s3-scan-tar.py
+++ s3-scan-tar/s3-scan-tar.py
@@ -20,29 +20,40 @@ from _version import __version__
 CLAMAVERROR = 10
 
 class BinaryFileLimitedOnSize(io.RawIOBase):
-    """ Subclassing the file/RawIOBase class to impose limits on file size by throwing EOF Exception
-    when we cross the limit.Used to handle the 4GB limit in clamav."""
+    """ Subclassing the file/RawIOBase class to impose limits on file size by returning short reads
+    when we cross the limit. Used to handle the 4GB limit in clamav."""
     def __init__(self, fh, maxsize=None):
         self.maxsize = maxsize
         self.fh = fh
+        self.restricted = False
 
     def write(self, s) -> int:
-        if self.maxsize and self.fh.tell() + len(s) > self.maxsize:
-            raise(EOFError("write would cross maxsize boundry"))
-        return self.fh.write(s)
+        raise(NotImplementedError("write() not supported"))
 
     def read(self, read_length) -> bytes:
+        # progress = self.fh.tell() / self.maxsize
+        # print(f'read({read_length} @ { progress:.2f})')
         if self.maxsize:
             if not read_length:
                 # no lenght given, not sure we if can read this without going over the limit
-                raise(EOFError("Unlimited read requested. Not supported"))
+                raise(NotImplementedError("Unlimited read requested. Not supported"))
             if self.fh.tell() + read_length >= self.maxsize:
-                raise(EOFError("read would cross maxsize boundry"))
+                logging.warning("Restricting file object and discarding the rest of the contents")
+                self.restricted = True
+                self.read_flush()
+                return None
+                # raise(EOFError("read would cross maxsize boundry"))
             else:
                 return self.fh.read(read_length)
     
+    def read_flush(self):
+        """ Read and discard the rest of the file. """
+        while len(self.fh.read(1024**2)) > 0: # read big chunks...
+            pass
+    
     def tell(self):
         return self.fh.tell()
+
     
     def seek(self, pos):
         raise(io.UnsupportedOperation)
@@ -112,19 +123,29 @@ with open(avlogfile,mode='w') as avlog:
         tar_member =  tf.extractfile(member)
         if tar_member == None:
             # Handle is none - likely a directory.
-            print("Handle is none. Skipping...")
+            logging.debug("Handle is none. Skipping...")
             continue
-        handle = BinaryFileLimitedOnSize(tar_member, 1024**3)
+        limit = int(os.getenv('MAXFILESIZE', 1023)) # size given in megabytes
+        handle = BinaryFileLimitedOnSize(tar_member, 1024**2 * limit)
         # print(handle.read())
         try:
+            logging.info(f'Scanning {member.name}...')
             result = cd.scan_stream(handle)
+            logging.info("...done")
             if (result is None):
                 print(f'OK - {member.name}', file=avlog)
             else:
                 print(f'Virus found! {result["stream"][1]} in {member.name}', file=avlog)
                 virus += 1
-        except EOFError as exception:
+            if handle.restricted:
+                logging.warning('Scan restricted by size limit.')
+                skipped += 1
+        except ConnectionResetError as exception:
+            logging.error('clamd reset the connection. Increase max scan size for clamd.')
+            logging.warning('Flushing the file.')
+            handle.read_flush()
             print(f'SKIPPED (File to big) - {member.name}', file=avlog)
+
             skipped += 1
         except Exception as e:
             logging.error(f"Failed to scan {member.name}")
@@ -133,9 +154,7 @@ with open(avlogfile,mode='w') as avlog:
     print("========", file=avlog)
     print(f"{virus} viruses found", file=avlog)
     print(f"{skipped} files skipped", file=avlog)
-    print(f"Archive scanned. Log created as {avlogfile}")
-
-
+    logging.info(f"Archive scanned. Log created as {avlogfile}")
 
 
 
