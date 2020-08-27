b60132be8a4f85175b6cc59191ff64c6415bfab7
Per Buer <per.buer@gmail.com>
2020-08-27 10:16:37 +0200

    logging

--- s3-delete/s3-delete.py
+++ s3-delete/s3-delete.py
@@ -3,20 +3,29 @@ import os
 import sys
 import logging
 from av_objectstore import ArkivverketObjectStorage
+from _version import __version__
 
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    print("Failed to load dotenv file. Assuming production")
 
+SUCCESS = 0
 ENVERROR = 1
 DELETEERROR = 2
 
 bucket = os.getenv('BUCKET')
 filename = os.getenv('OBJECT')
 
+logging.basicConfig(level=logging.INFO, 
+                    filemode='w', format='%(asctime)s %(levelname)s %(message)s')
+logging.info(f'{__file__} version {__version__} running')
+
 storage = ArkivverketObjectStorage()
 if storage.delete(bucket, filename):
     logging.info(f"Object deleted {filename} succesfully from {bucket}")
-    sys.exit(0)
+    sys.exit(SUCCESS)
 else:
     logging.error(f"Failed to delete {filename} from {bucket}")
     sys.exit(DELETEERROR)
