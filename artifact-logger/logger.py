c812f9c569e855b71f48a5eb754bbaf1d0672e7a
Per Buer <per.buer@gmail.com>
2020-09-05 09:20:03 +0200

    linting 10/10

--- artifact-logger/logger.py
+++ artifact-logger/logger.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python
+""" This container takes artifacts and logs them to the log-archive service """
+# pylint: disable=logging-fstring-interpolation,too-many-arguments
 
-import requests
-from requests.exceptions import HTTPError
-import os
-import uuid
+import base64
+import datetime
 import json
 import logging
-import base64
-import time
+import os
+import uuid
+
 import magic
-import datetime
+import requests
 from _version import __version__
 
 try:
     from dotenv import load_dotenv
     load_dotenv()
-except:
-    print('Could not load dotenv modules or .env file. Ignoring.')
+except ModuleNotFoundError:
+    print('Could not load python-dotenv. Production assumed.')
 
 
 def get_mime(path):
+    """ Fetches the mine type of a file """
     return magic.from_file(path, mime=True)
 
 
-def log(endpoint, token, uuid, path, name, mime, condition, message):
-    print(f'uuid: {uuid}, path: {path}, name: {name}, mime: {mime}')
+def log(endpoint, token, arch_uuid, path, name, mime, condition, message):
+    """ Logs to the remote log api """
+    print(f'uuid: {arch_uuid}, path: {path}, name: {name}, mime: {mime}')
 
     # Note that we need a string, which is why it is encoded as utf.
     with open(path, 'rb') as myfile:
         content = base64.b64encode(myfile.read()).decode('utf-8')
 
     log_obj = {
-        'archuuid': uuid,
+        'archuuid': arch_uuid,
         'sender': 'logger.py',
         'time_recorded': datetime.datetime.now().isoformat(),
         'message': message,
@@ -41,38 +44,36 @@ def log(endpoint, token, uuid, path, name, mime, condition, message):
         'attachment_name': name
     }
     response = requests.post(endpoint,
-                        headers={'access_token':token},
-                        data=json.dumps(log_obj)
-                        )
+                             headers={'access_token': token},
+                             data=json.dumps(log_obj)
+                             )
 
     # Bail if it fails.
     response.raise_for_status()
 
     logging.info(f'{name} logged OK')
-    
 
 
 def main():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')    
+    """ main method. Run from here unless imported"""
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(levelname)s %(message)s')
     logging.info(f'{__file__} version {__version__} running')
 
-
     files = os.getenv('FILES').split(';')
-    uuid = os.getenv('UUID')
+    arch_uuid = os.getenv('UUID')
     condition = os.getenv('CONDITION', 'ok')
     message = os.getenv('MESSAGE', '')
     baseurl = os.getenv('BASEURL', 'http://localhost:8000/')
     endpoint = baseurl + 'ingest'
     token = os.getenv('TOKEN', 'test_token')
-    for lfile in files:        
+    for lfile in files:
         mime = get_mime(lfile)
         name = os.path.basename(lfile)
-        logging.info(f'UUID({uuid}): Logging ({name}), type {mime} to {endpoint}')
-        log(endpoint, token, uuid, lfile, name, mime, condition, message)
+        logging.info(
+            f'UUID({uuid}): Logging ({name}), type {mime} to {endpoint}')
+        log(endpoint, token, arch_uuid, lfile, name, mime, condition, message)
 
 
 if __name__ == "__main__":
     main()
-
-
-
