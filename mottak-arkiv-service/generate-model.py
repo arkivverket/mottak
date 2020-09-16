4bd36dbf01e33d4600a0666bef71095b85fa48c0
Per Buer <per.buer@gmail.com>
2020-09-16 10:52:17 +0200

    Add a model generator

---
+++ mottak-arkiv-service/generate-model.py
@@ -0,0 +1,10 @@
+import os, sys
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+from app.db.schemas.mottak import Arkivuttrekk, Invitasjon, Lokasjon, Metadatafil, Overforingspakke, Tester
+from app.db.baseclass import Base
+target_metadata = Base.metadata
+
+from sqlalchemy_schemadisplay import create_schema_graph
+# create the pydot graph object by autoloading all tables via a bound metadata object
+graph = create_schema_graph(metadata=target_metadata)
+graph.write_png('dbschema.png') # write out the file
