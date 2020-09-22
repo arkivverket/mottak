02609947ea612d49fcc8981ab158e1c32fab91f7
Per Buer <per.buer@gmail.com>
2020-09-22 15:00:01 +0200

    pylint

--- storage-sas-generator/app/constants.py
+++ storage-sas-generator/app/constants.py
@@ -1,4 +1,4 @@
-
+""" Constants used in the SAS generator. Note what we star-import this, so don't pollute. """
 STATUS_OK = 0
 STATUS_ERROR = 1
 STATUS_INITIALIZING = 2
--- storage-sas-generator/app/model/global_state.py
+++ storage-sas-generator/app/model/global_state.py
@@ -1,12 +1,11 @@
-
+""" Global state handling."""
 from dataclasses import dataclass
 from azure.storage.blob.aio import BlobServiceClient
 
 
 @dataclass
-class GlobalState():
+class GlobalState:
+    """We keep global state in an object like this. """
     status_code: int
     status_message: str
     azure_client: BlobServiceClient
-
-
