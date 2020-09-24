2eab1ce204babe0ac1fb23989e5543d8e218209f
Mårten Elmgren <elmgren@gmail.com>
2020-09-24 23:34:52 +0200

    Mol-102: Add test for main method get_archive and health_check

--- mottak-arkiv-service/tests/test_main.py
+++ mottak-arkiv-service/tests/test_main.py
@@ -5,6 +5,19 @@ from app.main import app
 client = TestClient(app)
 
 # TODO Mock database in pytest
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == "Seems healthy"
+
+
+def test_get_archive():
+    response = client.get("/arkiv/2d105035-5900-43fe-ab4d-74d102866fe6")
+    assert response.status_code == status.HTTP_200_OK
+
+
 def test_get_archives():
     response = client.get("/arkiver")
     assert response.status_code == status.HTTP_200_OK
