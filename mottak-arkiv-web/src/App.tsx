fc8452572dc9ac95c36fde9ebd5e4125c9d906fe
Tuva Solstad <tuvsol@arkivverket.no>
2020-09-25 10:09:24 +0200

    MOL-116 Initial setup

---
+++ mottak-arkiv-web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import WorkArea from './components/WorkArea'
+
+const App: React.FC = () => {
+	return (
+		<WorkArea title={'trykk her'} />
+	)
+}
+
+export default App
---
+++ mottak-arkiv-web/src/components/WorkArea.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Button } from '@material-ui/core'
+import { getHeapCodeStatistics } from 'v8'
+
+
+type Props = {
+	title: string
+}
+
+const WorkArea: React.FC<Props> = ({
+	title
+}) => {
+
+	return (
+		<div>
+		</div>
+	)
+}
+
+export default WorkArea
