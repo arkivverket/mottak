bb16b39aaf77c1264372c376447db96ef4c26946
Tuva Solstad <tuvsol@arkivverket.no>
2020-09-25 10:11:15 +0200

    MOL-116 Setup test

--- mottak-arkiv-web/src/components/WorkArea.tsx
+++ mottak-arkiv-web/src/components/WorkArea.tsx
@@ -10,9 +10,29 @@ type Props = {
 const WorkArea: React.FC<Props> = ({
 	title
 }) => {
+	const [data, setData] = useState('Ingen data ennå')
+
+	const updateData = () => {
+		const getData = async () => {
+			try {
+				const response = await fetch('http://lego.arkivverket.local/bakom/public/api/index.php/metadata/municipalities')
+
+				const json = await response.json()
+				// console.log(json);
+				setData(JSON.stringify(json))
+			} catch (error) {
+				setData('Feil ved lasting av data')
+			}
+		}
+
+		console.log('clicked')
+		getData()
+	}
 
 	return (
 		<div>
+			<Button onClick={updateData} color="primary">{title}</Button>
+			<div>{data}</div>
 		</div>
 	)
 }
