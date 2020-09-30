bb4fc61b9b7a4204027c3b6d30673a7dbecc9ce7
Mårten Elmgren <elmgren@gmail.com>
2020-09-30 17:34:33 +0200

    Mol-120: Add test for upload_metadatafil

--- mottak-arkiv-service/tests/test_main.py
+++ mottak-arkiv-service/tests/test_main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from fastapi.testclient import TestClient
 from fastapi import status
 from app.main import app
@@ -13,6 +14,13 @@ def test_health_check():
     assert response.json() == "Seems healthy"
 
 
+def test_upload_metadatafil():
+    file_path = Path.cwd() / 'testdata' / 'metadatafil1.xml'
+    with open(file_path, 'rb') as f:
+        response = client.post("metadatafil", files={"file": ("metadatafile.xml", f, "text/xml")})
+    assert response.status_code == status.HTTP_201_CREATED
+
+
 def test_get_archive():
     response = client.get("/arkiv/1")
     assert response.status_code == status.HTTP_200_OK
@@ -21,4 +29,3 @@ def test_get_archive():
 def test_get_archives():
     response = client.get("/arkiver")
     assert response.status_code == status.HTTP_200_OK
-
