0666650eb445e52ef15395cbdc906fb75139dbf9
Mårten Elmgren <elmgren@gmail.com>
2020-10-01 14:26:54 +0200

    Mol-120: Add new path operation function for uploading metadata in main and corresponding create method in repository

--- mottak-arkiv-service/app/db/repository.py
+++ mottak-arkiv-service/app/db/repository.py
@@ -1,7 +1,7 @@
-from uuid import UUID
 from sqlalchemy import desc
 from sqlalchemy.orm import Session
-from app.db.schemas.mottak import Arkivuttrekk
+from app.db.schemas.mottak import Arkivuttrekk, Metadatafil
+from app.dto.Metadatafil import BaseMetadatafil
 
 
 def get_arkivuttrekk(db: Session, arkivuttrekk_id: int):
@@ -12,3 +12,9 @@ def get_all_arkivuttrekk(db: Session, skip: int = 0, limit: int = 10):
     return db.query(Arkivuttrekk) \
         .order_by(desc(Arkivuttrekk.opprettet)).offset(skip).limit(limit).all()
 
+
+def create_metadatafil(db: Session, base_metadatafil: BaseMetadatafil) -> int:
+    db_metadatafil = Metadatafil(**base_metadatafil.dict())
+    db.add(db_metadatafil)
+    db.commit()
+    return db_metadatafil.id
--- mottak-arkiv-service/app/main.py
+++ mottak-arkiv-service/app/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from typing import List
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, File, UploadFile
 from sqlalchemy.orm import Session
 from app.db.database import get_session
-from app.db.repository import get_arkivuttrekk, get_all_arkivuttrekk
+from app.db.repository import get_arkivuttrekk, get_all_arkivuttrekk, create_metadatafil
 from app.dto.Arkivuttrekk import Arkivuttrekk
+from app.dto.Metadatafil import BaseMetadatafil
 
 try:
     from dotenv import load_dotenv
@@ -38,6 +39,18 @@ async def health_check():
     return "Seems healthy"
 
 
+@app.post("/metadatafil",
+          status_code=status.HTTP_201_CREATED,
+          response_model=int,
+          tags=["metadata"],
+          summary="Laste opp en metadatafil")
+async def upload_metadatafil(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    # Read file content (XML) as utf-8 and remove newlines
+    xmlstring = file.file.read().decode('utf-8').replace('\n', '')
+    metadatafil = BaseMetadatafil(file.filename, file.content_type, xmlstring)
+    return create_metadatafil(db, metadatafil)
+
+
 @app.get("/arkiv/{id}",
          status_code=status.HTTP_200_OK,
          response_model=Arkivuttrekk,
@@ -52,7 +65,7 @@ async def get_archive(id: int,  db: Session = Depends(get_db)):
          response_model=List[Arkivuttrekk],
          tags=["arkivuttrekk"],
          summary="Hent alle arkivuttrekk")
-def get_archives(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
+async def get_archives(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
     return get_all_arkivuttrekk(db, skip, limit)
 
 
