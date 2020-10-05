7de1bdeb5fa36d8808b5e9d23be7b99c41adba26
Mårten Elmgren <elmgren@gmail.com>
2020-10-05 15:04:36 +0200

    Mol-120: Add return type in repository methods

--- mottak-arkiv-service/app/db/repository.py
+++ mottak-arkiv-service/app/db/repository.py
@@ -1,20 +1,22 @@
+from typing import List
+
 from sqlalchemy import desc
 from sqlalchemy.orm import Session
 from app.db.schemas.mottak import Arkivuttrekk, Metadatafil
 from app.dto.Metadatafil import BaseMetadatafil
 
 
-def get_arkivuttrekk(db: Session, arkivuttrekk_id: int):
+def get_arkivuttrekk(db: Session, arkivuttrekk_id: int) -> Arkivuttrekk:
     return db.query(Arkivuttrekk).get(arkivuttrekk_id)
 
 
-def get_all_arkivuttrekk(db: Session, skip: int = 0, limit: int = 10):
+def get_all_arkivuttrekk(db: Session, skip: int = 0, limit: int = 10) -> List[Arkivuttrekk]:
     return db.query(Arkivuttrekk) \
         .order_by(desc(Arkivuttrekk.opprettet)).offset(skip).limit(limit).all()
 
 
-def create_metadatafil(db: Session, base_metadatafil: BaseMetadatafil) -> int:
+def create_metadatafil(db: Session, base_metadatafil: BaseMetadatafil) -> Metadatafil:
     db_metadatafil = Metadatafil(**base_metadatafil.dict())
     db.add(db_metadatafil)
     db.commit()
-    return db_metadatafil.id
+    return db_metadatafil
