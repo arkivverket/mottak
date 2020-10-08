427b0fb012f14e93bfd225c7afbc0a758b2772e4
Mårten Elmgren <elmgren@gmail.com>
2020-10-08 10:37:07 +0200

    Mol-120: Refactor main.py to use routers

--- mottak-arkiv-service/app/main.py
+++ mottak-arkiv-service/app/main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-from typing import List
-from fastapi import FastAPI, Depends, Request, status, File, UploadFile
+from fastapi import FastAPI, status
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session, exc
-from app.db.database import get_session
-from app.db.repository import get_arkivuttrekk, get_all_arkivuttrekk, create_metadatafil
-from app.dto.Arkivuttrekk import Arkivuttrekk
-from app.dto.Metadatafil import BaseMetadatafil, Metadatafil
+from sqlalchemy.orm import exc
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+from app.routers import arkivuttrekk, metadatafil
 
 try:
     from dotenv import load_dotenv
+
     load_dotenv()
 except ModuleNotFoundError:
     pass
@@ -22,17 +21,10 @@ app = FastAPI(
 )
 
 
-# Depdendency
-def get_db():
-    try:
-        db = get_session()
-        yield db
-    finally:
-        db.close()
-
-
+# TODO Fix Exception handler
+# Exception handlers
 @app.exception_handler(exc.NoResultFound)
-async def sqlalchemy_exception_handler(request: Request, exception: exc.NoResultFound):
+def sqlalchemy_exception_handler(request: Request, exception: exc.NoResultFound):
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
         content=jsonable_encoder(
@@ -45,37 +37,16 @@ async def sqlalchemy_exception_handler(request: Request, exception: exc.NoResult
          status_code=status.HTTP_200_OK,
          tags=["health"],
          summary="Helsesjekk av applikasjonen")
-async def health_check():
+def health_check():
     return "Seems healthy"
 
 
-@app.post("/metadatafil",
-          status_code=status.HTTP_201_CREATED,
-          response_model=Metadatafil,
-          tags=["metadata"],
-          summary="Laste opp en metadatafil")
-async def upload_metadatafil(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    # Read file content (XML) as utf-8 and remove newlines
-    xmlstring = file.file.read().decode('utf-8').replace('\r\n', '')
-    metadatafil = BaseMetadatafil(file.filename, file.content_type, xmlstring)
-    return create_metadatafil(db, metadatafil)
-
-
-@app.get("/arkivuttrekk/{id}",
-         status_code=status.HTTP_200_OK,
-         response_model=Arkivuttrekk,
-         tags=["arkivuttrekk"],
-         summary="Hent arkivuttrekk basert på id")
-async def get_archive(id: int,  db: Session = Depends(get_db)):
-    return get_arkivuttrekk(db, id)
-
-
-@app.get("/arkivuttrekk",
-         status_code=status.HTTP_200_OK,
-         response_model=List[Arkivuttrekk],
-         tags=["arkivuttrekk"],
-         summary="Hent alle arkivuttrekk")
-async def get_archives(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
-    return get_all_arkivuttrekk(db, skip, limit)
-
-
+app.include_router(
+    arkivuttrekk.router,
+    prefix="/arkivuttrekk",
+    tags=['arkivuttrekk'])
+app.include_router(
+    metadatafil.router,
+    prefix="/metadatafil",
+    tags=['metadatafil'])
+# app.add_exception_handler(exc.NoResultFound, sqlalchemy_exception_handler)
