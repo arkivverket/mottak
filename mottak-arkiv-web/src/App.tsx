fad05a1e3b514bfe2424edf8534a90ebf500a87c
Tuva Solstad <tuvsol@arkivverket.no>
2020-10-13 14:08:40 +0200

    MOL-140 Changed from styled-components to material-ui styling + forgot file

--- mottak-arkiv-web/src/App.tsx
+++ mottak-arkiv-web/src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import GlobalStyle from './styles/globalStyles'
+import { BrowserRouter } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Layout from './components/layout/Layout'
-import Routes, { RouteType } from './components/routes/Routes'
 import { defaultTheme } from './styles/themes'
-import { ThemeProvider } from 'styled-components'
-import WorkArea from './components/WorkArea'
+import { ThemeProvider } from '@material-ui/core/styles'
 
 const App: React.FC = (): JSX.Element => (
 	<ThemeProvider theme={defaultTheme}>
-		<GlobalStyle />
 		<BrowserRouter>
-			<div>
-				<Layout />
-			</div>
+			<CssBaseline />
+			<Layout />
 		</BrowserRouter>
 	</ThemeProvider>
 
