46baf7eb111278712005a5ed137a4747361e4801
Tuva Solstad <tuvsol@arkivverket.no>
2020-10-13 14:42:15 +0200

    MOL-140 Added shared styles

---
+++ mottak-arkiv-web/src/styles/sharedStyles.tsx
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useSharedStyles = makeStyles(theme => ({
+	roundedBox: {
+		padding: theme.spacing(3),
+		borderRadius: theme.spacing(1),
+		border: `1px solid ${theme.palette.secondary.main}`,
+	},
+	styledForm: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		width: '100%',
+	},
+	buttonDA: {
+		backgroundColor: 'white',
+		color: theme.palette.primary.dark,
+		'&:hover': {
+			backgroundColor: theme.palette.secondary.main,
+		}
+	},
+}))
+
