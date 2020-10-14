e1eef12a93d122c21e72f6d479ca39b58b27f539
Mårten Elmgren <elmgren@gmail.com>
2020-10-14 11:13:15 +0200

    Mol-121: Add common test fixtures

---
+++ mottak-arkiv-service/tests/common_fixtures.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi import UploadFile
+
+from utils import get_project_root
+
+
+@pytest.fixture(scope='module')
+def testfile():
+    file_path = get_project_root() / 'tests' / 'testdata' / 'metadatafil1.xml'
+    with open(file_path, 'rb') as f:
+        uploadfile = UploadFile(filename='df53d1d8-39bf-4fea-a741-58d472664ce2.xml', content_type='text/xml', file=f)
+        yield uploadfile
+
+
+@pytest.fixture(scope='module')
+def testfile_with_errors():
+    file_path = get_project_root() / 'tests' / 'testdata' / 'metadatafil1_errors.xml'
+    with open(file_path, 'rb') as fe:
+        uploadfile = UploadFile(filename='df53d1d8-39bf-4fea-a741-58d472664ce2.xml', content_type='text/xml', file=fe)
+        yield uploadfile
