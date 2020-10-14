4db4faa64a48344018a285cbf1d556bd6b49dc05
Kristoffer Waløen <kriwal@arkivverket.no>
2020-10-14 16:42:21 +0200

    Cleans up setup script

--- azure-pipelines/run_helm_dev.sh
+++ azure-pipelines/run_helm_dev.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 if [ -z "$1" ]
   then
@@ -9,20 +9,9 @@ if [ -z "$2" ]
     echo "Please use scripts as run_helm.sh <env> <output_location>"
 fi
 ENV=$1:l
-OUTPUT_FOLDER="$2/$ENV"
+OUTPUT_FOLDER="$2"
 
-
-CLUSTER_NAME="av-plattform-$ENV-aks"
-RESOURCE_GROUP="av-plattform-$ENV-rg"
-
-
-
-## Auth against cluster
-#az aks get-credentials --name "$CLUSTER_NAME" --resource-group "$RESOURCE_GROUP"
 
 ## Build yaml files
 helm template --values "values-$ENV.yaml" --output-dir "$OUTPUT_FOLDER" k8s/
 #helm template --values "values-$ENV.yaml" --set AV_NAMESPACE=testingIt --output-dir "$OUTPUT_FOLDER" k8s/ # Test for replacing values
-
-## Deploy yaml files
-#kubectl apply -f
--- azure-pipelines/setup.sh
+++ azure-pipelines/setup.sh
@@ -1,14 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 # Sets up things i mottak namespace
 
+# Change this to match current env.
+# Expects to find a values-<env>.yaml file in same folder as this script.
 ENV="dev"
-OUTPUT_FOLDER="k8s/output"
 
+
+OUTPUT_FOLDER="k8s/output/$ENV"
+CLUSTER_NAME="av-plattform-$ENV-aks"
+RESOURCE_GROUP="av-plattform-$ENV-rg"
+NAMESPACE="av-mottak-$ENV"
+
+## Auth against cluster
+echo
+echo "Setting up aks credentials"
+az aks get-credentials --name "$CLUSTER_NAME" --resource-group "$RESOURCE_GROUP"
+
+# Installing argo workflows in namespace
+echo
+echo "Installing argo in namespace: $NAMESPACE"
+kubectl apply -n "$NAMESPACE" -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
+
+
+# Build helm tmplates and apply to cluster
+OUTPUT_TEMPLATE_FOLDER="$OUTPUT_FOLDER/av-mottak/templates/"
+
+echo
+echo "Building helm templates"
 zsh run_helm_dev.sh $ENV $OUTPUT_FOLDER
 
-TEMPLATE_FOLDER="$OUTPUT_FOLDER/$ENV/av-mottak/templates/"
-
-
-kubectl apply -f "$TEMPLATE_FOLDER/csi-driver-link.yaml"
-kubectl apply -f "$TEMPLATE_FOLDER/mount_the_secrets.yaml"
-kubectl apply -f "$TEMPLATE_FOLDER/minio-deployment.yaml"
+echo
+echo "Running kubectl apply -f on setup yamls"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/csi-driver-link.yaml"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/mount_the_secrets.yaml"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/minio-deployment.yaml"
