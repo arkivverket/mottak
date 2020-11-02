31f8e1bb9629b4d4122ce3a8b48375e3a4a72a9d
Tuva Solstad <tuvsol@arkivverket.no>
2020-11-02 08:56:59 +0100

    Arkivuttrekk-request and display test

--- mottak-arkiv-web/src/__test__/integration/Overview.spec.tsx
+++ mottak-arkiv-web/src/__test__/integration/Overview.spec.tsx
@@ -3,8 +3,49 @@ import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import Overview from '../../components/Overview'
+import useRequest from '../../hooks/useRequest'
+
+import '@testing-library/jest-dom/extend-expect'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+jest.mock('../../hooks/useRequest.ts')
+
+const arkivuttrekk = [{
+	id: 1,
+	opprettet: null,
+	endret: null,
+	obj_id: '1',
+	status: 'Invitert',
+	type: 'Noark3',
+	tittel: 'En tittel',
+	sjekksum_sha256: '11111',
+	avgiver_navn: 'Noen',
+	avgiver_epost: 'noen@stderr.no',
+	koordinator_epost: 'andre@stderr.no',
+	metadatafil_id: 1,
+	arkiv_startdato: null,
+	arkiv_sluttdato: null,
+	storrelse: '1',
+	avtalenummer: '1',
+}]
+
+useRequest.mockReturnValue({
+	data: arkivuttrekk,
+	loading: false,
+	error: null,
+	performRequest: jest.fn()
+})
 
 describe('<Overview />', () => {
+	const server = setupServer(
+		rest.get('http://localhost:8000/arkivuttrekk', (req, res, ctx) => res(ctx.json(arkivuttrekk)))
+	)
+
+	// establish API mocking before all tests
+	beforeAll(() => server.listen())
+	afterEach(() => server.resetHandlers())
+	afterAll(() => server.close())
 
 	it('has button that will start add new AU workflow', () => {
 		const history = createMemoryHistory()
@@ -21,4 +62,15 @@ describe('<Overview />', () => {
 	})
 
 
+	it('loads and displays arkivuttrekk', async () => {
+		const history = createMemoryHistory()
+		render(
+			<Router history={history}>
+				<Overview />
+			</Router>
+		)
+
+
+		await waitFor(() => screen.getByText(/en tittel/i))
+	})
 })
