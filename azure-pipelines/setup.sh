64de7c02a10136b3b9c9e31ef0a2c14a279b12df
Kristoffer Waløen <kriwal@arkivverket.no>
2020-12-14 13:37:45 +0100

    Changes av with da

--- azure-pipelines/setup.sh
+++ azure-pipelines/setup.sh
@@ -9,10 +9,10 @@ ENV="dev"
 OUTPUT_FOLDER="k8s/output/$ENV"
 CLUSTER_NAME="av-plattform-$ENV-aks"
 PLATFORM_RESOURCE_GROUP="av-plattform-$ENV-rg"
-MOTTAK_RESOURCE_GROUP="av-mottak-$ENV-rg"
-NAMESPACE="av-mottak-$ENV"
-STORAGE_ACCOUNT_NAME="avmottak${ENV}sacc"
-MOTTAK_VAULT_NAME="av-mottak-$ENV-vault"
+MOTTAK_RESOURCE_GROUP="da-mottak-$ENV-rg"
+NAMESPACE="da-mottak-$ENV"
+STORAGE_ACCOUNT_NAME="damottak${ENV}sa"
+MOTTAK_VAULT_NAME="da-mottak-$ENV-vault"
 
 ## Auth against cluster
 echo
@@ -22,33 +22,33 @@ az aks get-credentials --name "$CLUSTER_NAME" --resource-group "$PLATFORM_RESOUR
 # Installing argo workflows in namespace
 echo
 echo "Installing argo in namespace: $NAMESPACE"
-kubectl apply -n "$NAMESPACE" -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
+#kubectl apply -n "$NAMESPACE" -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
 
 
 # Build helm templates and apply to cluster
-OUTPUT_TEMPLATE_FOLDER="$OUTPUT_FOLDER/av-mottak/templates/"
+OUTPUT_TEMPLATE_FOLDER="$OUTPUT_FOLDER/da-mottak/templates/"
 
 echo
 echo "Building helm templates"
 zsh run_helm_dev.sh $ENV $OUTPUT_FOLDER
 
-echo
-echo "Running kubectl apply -f on setup yamls"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/csi-driver-link.yaml"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/mount_the_secrets.yaml"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/minio-deployment.yaml"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/argo-rbac.yaml"
-
-echo
-echo "Setting up ingress"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/ingress.yaml"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/nginx.yaml"
-kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/oauth2-proxy.yaml"
+#echo
+#echo "Running kubectl apply -f on setup yamls"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/csi-driver-link.yaml"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/mount_the_secrets.yaml"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/minio-deployment.yaml"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/argo-rbac.yaml"
+#
+#echo
+#echo "Setting up ingress"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/ingress.yaml"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/nginx.yaml"
+#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/oauth2-proxy.yaml"
 
 #echo
-#echo "Setting up av-mottak-$ENV-rg"
+#echo "Setting up da-mottak-$ENV-rg"
 #az storage container create --name "tusd-target" --account-name $STORAGE_ACCOUNT_NAME --resource-group $MOTTAK_RESOURCE_GROUP
 
 #echo
-#echo "Setting up av-mottak-$ENV-vault"
+#echo "Setting up da-mottak-$ENV-vault"
 #az keyvault secret set --name "mailgun-secret" --vault-name "$MOTTAK_VAULT_NAME" --description "Secret for using mailgun API"
