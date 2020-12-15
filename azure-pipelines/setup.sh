31790059f0428ee61de374a2ff06a2fabc06da07
Kristoffer Waløen <kriwal@arkivverket.no>
2020-12-15 10:30:16 +0100

    Updates container tags in values file and uncomments code in setup.sh

--- azure-pipelines/setup.sh
+++ azure-pipelines/setup.sh
@@ -19,12 +19,6 @@ echo
 echo "Setting up aks credentials for cluster $CLUSTER_NAME in resource group $PLATFORM_RESOURCE_GROUP"
 az aks get-credentials --name "$CLUSTER_NAME" --resource-group "$PLATFORM_RESOURCE_GROUP"
 
-# Installing argo workflows in namespace
-echo
-echo "Installing argo in namespace: $NAMESPACE"
-#kubectl apply -n "$NAMESPACE" -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
-
-
 # Build helm templates and apply to cluster
 OUTPUT_TEMPLATE_FOLDER="$OUTPUT_FOLDER/da-mottak/templates/"
 
@@ -32,23 +26,20 @@ echo
 echo "Building helm templates"
 zsh run_helm_dev.sh $ENV $OUTPUT_FOLDER
 
-#echo
-#echo "Running kubectl apply -f on setup yamls"
-#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/csi-driver-link.yaml"
-#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/mount_the_secrets.yaml"
-#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/minio-deployment.yaml"
-#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/argo-rbac.yaml"
-#
-#echo
-#echo "Setting up ingress"
-#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/ingress.yaml"
-#kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/nginx.yaml"
+echo
+echo "Running kubectl apply -f on setup yamls"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/csi-driver-link.yaml"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/mount_the_secrets.yaml"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/minio-deployment.yaml"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/argo-rbac.yaml"
+
+echo
+echo "Setting up ingress"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/ingress.yaml"
+kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/nginx.yaml"
 #kubectl apply -f "$OUTPUT_TEMPLATE_FOLDER/oauth2-proxy.yaml"
 
-#echo
-#echo "Setting up da-mottak-$ENV-rg"
-#az storage container create --name "tusd-target" --account-name $STORAGE_ACCOUNT_NAME --resource-group $MOTTAK_RESOURCE_GROUP
-
-#echo
-#echo "Setting up da-mottak-$ENV-vault"
-#az keyvault secret set --name "mailgun-secret" --vault-name "$MOTTAK_VAULT_NAME" --description "Secret for using mailgun API"
+# Installing argo workflows in namespace
+echo
+echo "Installing argo in namespace: $NAMESPACE"
+kubectl apply -n "$NAMESPACE" -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
