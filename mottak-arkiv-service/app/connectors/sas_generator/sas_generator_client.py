71b2c44efb96d3bee33598997b79a17069ccf5bd
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-07 09:37:57 +0100

    MOL-162 Fix open/close warning in HTTPX AsyncClient for SAS Generator

--- mottak-arkiv-service/app/connectors/sas_generator/sas_generator_client.py
+++ mottak-arkiv-service/app/connectors/sas_generator/sas_generator_client.py
@@ -3,12 +3,11 @@ from uuid import UUID
 
 from app.connectors.sas_generator.models import SASTokenRequest
 
-class SASGeneratorClient(AsyncClient):
-    def __init__(self, sas_url: str,):
-        super().__init__()
+class SASGeneratorClient():
+    def __init__(self, sas_url: str):
         self.url = sas_url
 
     async def request_sas(self, container: UUID, duration: int = 24):
-        request = SASTokenRequest(container, duration)
-        resp = await self.post(self.url, data=request.as_data())
-        return resp.json()
+        async with AsyncClient() as client:
+            request = SASTokenRequest(container, duration)
+            return await client.post(self.url, data=request.as_data())
