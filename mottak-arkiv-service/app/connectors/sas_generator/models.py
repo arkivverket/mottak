29bcc76d4ba3b7a28b9c6d8cff893b3647d1254c
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-11 13:25:34 +0100

    MOL-162 Adjust SASTokenRequest model to use the correct container name

--- mottak-arkiv-service/app/connectors/sas_generator/models.py
+++ mottak-arkiv-service/app/connectors/sas_generator/models.py
@@ -3,22 +3,16 @@ import json
 from pydantic.main import BaseModel
 from uuid import UUID
 
-
-class UUIDEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, UUID):
-            # if the obj is uuid, we simply return the value of uuid
-            return str(obj)
-        return json.JSONEncoder.default(self, obj)
+ZERO_GENERATION = '0'
 
 
 class SASTokenRequest:
     def __init__(self, container: UUID, duration: int):
-        self.container = container
+        self.container = f'{str(container)}-{ZERO_GENERATION}'
         self.duration = duration
 
     def as_json(self) -> str:
-        return json.dumps(self.__dict__, cls=UUIDEncoder, default=str)
+        return json.dumps(self.__dict__)
 
 
 class SASResponse(BaseModel):
