d3f5d58c0d8e35864b32e627e83ea81d9066a9a4
Mårten Elmgren <elmgren@gmail.com>
2021-01-12 11:28:40 +0100

    MOL-164: Add specific classes for AzureQueueSender and AzureQueueReceiver to avoid sending messages on a receiver and vice versa

--- mottak-arkiv-service/app/connectors/azure_servicebus/azure_servicebus_client.py
+++ mottak-arkiv-service/app/connectors/azure_servicebus/azure_servicebus_client.py
@@ -6,17 +6,31 @@ from azure.servicebus.common.errors import MessageSendFailed
 
 class AzureServicebus():
     def __init__(self, connection_string: str, queue_name: str):
+        self.queue_name = queue_name
         self.queue_client = self.create_queue_client(connection_string, queue_name)
 
     @staticmethod
     def create_queue_client(queue_client_string: str, queue_name: str) -> QueueClient:
         return QueueClient.from_connection_string(queue_client_string, queue_name)
 
+
+class AzureQueueReceiver(AzureServicebus):
+    def __init__(self, connection_string: str, queue_name: str):
+        super().__init__(connection_string, queue_name)
+        # self.receiver = self.queue_client.get_receiver()
+
+
+class AzureQueueSender(AzureServicebus):
+    def __init__(self, connection_string: str, queue_name: str):
+        super().__init__(connection_string, queue_name)
+        # self.sender = self.queue_client.get_sender()
+
     async def send_message(self, _message: str) -> bool:
         message = Message(_message)
 
         try:
             await self.queue_client.send(message)
+            # await self.sender.send(message)
         except MessageSendFailed:
             logging.error(f"Could not send message to the {self.queue_name} queue with the following data: {_message}")
             return False
