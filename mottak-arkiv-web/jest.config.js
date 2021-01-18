15d07a1fbc20ae9acfb3dc60c2bc06bb57a66064
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-18 18:05:58 +0100

    Improve testing configuration

--- mottak-arkiv-web/jest.config.js
+++ mottak-arkiv-web/jest.config.js
@@ -1,5 +1,19 @@
 module.exports = {
+	roots: ['<rootDir>/'],
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['<rootDir>/src/__test__/config/importJestDOM.ts'],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+	moduleDirectories: ['node_modules', 'src', 'test'],
+	moduleNameMapper: {
+		'\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+		'\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__test__/__mocks__/fileMock.js',
+	},
+	testPathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/src/__test__/(.eslintrc.js|jest.setup.ts)',
+		'<rootDir>/src/__test__/__mocks__/',
+	],
+	transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+	setupFilesAfterEnv: ['<rootDir>/src/__test__/jest.setup.ts'],
 }
