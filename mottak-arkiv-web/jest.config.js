fd4ca153e40d1068dea5c395f5aceb4ac169009e
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-19 09:03:16 +0100

    Rename the test directory to match Jest's default (`__tests__`)

--- mottak-arkiv-web/jest.config.js
+++ mottak-arkiv-web/jest.config.js
@@ -6,14 +6,14 @@ module.exports = {
 	moduleDirectories: ['node_modules', 'src', 'test'],
 	moduleNameMapper: {
 		'\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-		'\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__test__/__mocks__/fileMock.js',
+		'\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__tests__/__mocks__/fileMock.js',
 	},
 	testPathIgnorePatterns: [
 		'<rootDir>/node_modules/',
-		'<rootDir>/src/__test__/(.eslintrc.js|jest.setup.ts)',
-		'<rootDir>/src/__test__/__mocks__/',
+		'<rootDir>/src/__tests__/(.eslintrc.js|jest.setup.ts)',
+		'<rootDir>/src/__tests__/__mocks__/',
 	],
 	transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
 	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
-	setupFilesAfterEnv: ['<rootDir>/src/__test__/jest.setup.ts'],
+	setupFilesAfterEnv: ['<rootDir>/src/__tests__/jest.setup.ts'],
 }
