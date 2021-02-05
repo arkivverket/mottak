e40d3b7e3e5ed2875f28cf21ea123efada38dda0
Mårten Elmgren <elmgren@gmail.com>
2021-02-05 15:35:14 +0100

    MOL-285: Add parameterclass HookData

---
+++ tusd/hooks/models/HookData.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+
+class HookData:
+    """
+    Parameter class for data read from the hook event (stdin)
+    """
+    def __init__(self,
+                 tusd_id=None,
+                 ekstern_id=None,
+                 transferred_bytes=0,
+                 objekt_navn=None):
+        self.tusd_id = tusd_id
+        self.ekstern_id = ekstern_id
+        self.transferred_bytes = transferred_bytes
+        self.objekt_navn = objekt_navn
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, HookData):
+            return self.tusd_id == other.tusd_id and \
+                   self.ekstern_id == other.ekstern_id and \
+                   self.transferred_bytes == other.transferred_bytes and \
+                   self.objekt_navn == other.objekt_navn
+        return False
+
+    @staticmethod
+    def init_from_dict(json_dict: dict) -> HookData:
+        tusd_id = json_dict.get('Upload').get('ID')
+        ekstern_id = json_dict.get('Upload').get('MetaData').get('invitasjonEksternId')
+        transferred_bytes = json_dict.get('Upload').get('Offset')
+        objekt_navn = json_dict.get('Upload').get('Storage').get('Key')
+
+        return HookData(tusd_id=tusd_id,
+                        ekstern_id=ekstern_id,
+                        transferred_bytes=transferred_bytes,
+                        objekt_navn=objekt_navn)
