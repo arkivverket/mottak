73a98660889782071333d503269065fe2a4ff5df
Mårten Elmgren <elmgren@gmail.com>
2021-02-08 14:38:13 +0100

    MOL-287: Use new params in checksum.py

--- s3-checksum256/checksum.py
+++ s3-checksum256/checksum.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from __future__ import with_statement
 
-import os
-import sys
-import logging
 import hashlib
+import logging
+import os
+
 from py_objectstore import ArkivverketObjectStorage
 from _version import __version__
 
@@ -22,21 +22,24 @@ FILEERROR = 11
 RESULT = '/tmp/result'
 LOG = '/tmp/checksum.log'
 
+
 def object_checksum(obj):
     sha256_hash = hashlib.sha256()
     for byte_block in obj:
-            sha256_hash.update(byte_block)
+        sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
+
 
 # Not sure if this is relevant anymore....
 def write_result(res):
     with open(RESULT, "w") as res_file:
         res_file.write(res)
 
+
 def get_object_stream():
     bucket = os.getenv('BUCKET')
-    filename = os.getenv('OBJECT')
-    logging.info(f'Opening a streaming connection to {filename} in {bucket}')    
+    filename = os.getenv('TUSD_OBJECT_NAME')
+    logging.info(f'Opening a streaming connection to {filename} in {bucket}')
     storage = ArkivverketObjectStorage()
     return storage.download_stream(bucket, filename)
 
@@ -45,6 +48,7 @@ def main():
     logging.basicConfig(level=logging.INFO, filename=LOG,
                         filemode='w', format='%(asctime)s %(levelname)s %(message)s')
     logging.getLogger().addHandler(logging.StreamHandler())
+    logging.info(f'Starting s3-checksum256')
     logging.info(f'{__file__} version {__version__} running')
 
     try:
@@ -57,7 +61,7 @@ def main():
     except Exception as e:
         logging.error(f'error caught while streaming/checksumming: {e}')
         exit(FILEERROR)
-        
+
     expected = os.getenv('CHECKSUM')
     if checksum == expected:
         logging.info(f'Checksum ({checksum}) verified')
@@ -66,6 +70,7 @@ def main():
         logging.warning(f"Expected checksum {expected} doesn't match calculated {checksum}")
         write_result('mismatch')
         exit(CHECKSUMERROR)
+
 
 if __name__ == "__main__":
     main()
