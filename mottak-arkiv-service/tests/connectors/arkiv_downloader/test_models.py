ddede987ed900ccdd1084fa86585411f4ebb8fc0
Mårten Elmgren <elmgren@gmail.com>
2021-02-25 22:02:39 +0100

    MOL-326: Update tests for ArkivkopiRequest

--- mottak-arkiv-service/tests/connectors/arkiv_downloader/test_models.py
+++ mottak-arkiv-service/tests/connectors/arkiv_downloader/test_models.py
@@ -1,45 +1,57 @@
 import json
 import pytest
-from app.connectors.arkiv_downloader.models import ArkivkopiRequest, ArkivkopiStatusResponse
+from app.connectors.arkiv_downloader.models import ArkivkopiRequest, ArkivkopiStatusResponse, ArkivkopiRequestBlobInfo
 from app.connectors.sas_generator.models import SASResponse
 from app.domain.models.Arkivkopi import ArkivkopiStatus
 
 
 arkivkopi_id = 1
-storage_account = "dasaccdev"
-container = "33e8d091-48fd-4718-9c7a-abb9a266ee8f"
-sas_token = "st=2021-01-28T12%3A44%3A26Z&se=2021-01-29T00%3A59%3A26Z&sp=rl&sv=2020-02-10&sr=c&sig=IGAxVlPEF1Y%2B6Xh9mCEa90Dtq8VXP3GVJaa2f/5P%2BMA%3D"
+storage_account = "storage_account_test"
+container = "container_test"
+sas_token = "se=2020-12-05T14%3A40%3A54Z&sp=r&sv=2020-02-10&sr=c&sig=someSignature"
+source_name = "some/random/file"
+target_name = "target_filename.tar"
 status = ArkivkopiStatus.BESTILT
 
-@pytest.fixture()
-def arkivkopi_request():
-    return ArkivkopiRequest(arkivkopi_id, storage_account, container, sas_token)
 
-@pytest.fixture()
+@pytest.fixture
+def testobj_arkivkopi_request_with_blob(testobj_arkivkopi_request) -> ArkivkopiRequest:
+    blob_info = ArkivkopiRequestBlobInfo(source_name="some/random/file", target_name="target_filename.tar")
+    testobj_arkivkopi_request.blob_info = blob_info
+    return testobj_arkivkopi_request
+
+
+@pytest.fixture
 def arkivkopi_status_response():
     return ArkivkopiStatusResponse(arkivkopi_id, status)
 
 
-def test_arkivkopirequest_equals(arkivkopi_request):
-    expected = ArkivkopiRequest(arkivkopi_id, storage_account, container, sas_token)
-    assert expected == arkivkopi_request
+def test_arkivkopirequest_equals(testobj_arkivkopi_request):
+    result = ArkivkopiRequest(arkivkopi_id, storage_account, container, sas_token)
+    assert result == testobj_arkivkopi_request
 
 
-def test_arkivkopirequest_not_equals(arkivkopi_request):
+def test_arkivkopirequest_not_equals(testobj_arkivkopi_request):
     arkivkopi_request_2 = ArkivkopiRequest(arkivkopi_id+1, storage_account, container, sas_token)
-    assert arkivkopi_request != arkivkopi_request_2
+    assert testobj_arkivkopi_request != arkivkopi_request_2
 
 
-def test_arkivkopirequest_from_id_and_token(arkivkopi_request):
+def test_arkivkopirequest_from_id_and_token(testobj_arkivkopi_request):
     sas_response = SASResponse(storage_account, container, sas_token)
     result = ArkivkopiRequest.from_id_and_token(arkivkopi_id, sas_response)
-    assert arkivkopi_request == result
+    assert testobj_arkivkopi_request == result
 
 
-def test_arkivkopirequest_as_json(arkivkopi_request):
-    result = arkivkopi_request.as_json_str()
-    expected = json.dumps(arkivkopi_request.__dict__)
+def test_arkivkopirequest_as_json(testobj_arkivkopi_request):
+    result = testobj_arkivkopi_request.as_json_str()
+    expected = json.dumps(testobj_arkivkopi_request.__dict__)
     assert expected == result
+
+
+def test_arkivkopirequest_with_blob_info(testobj_arkivkopi_request_with_blob):
+    blob_info = {"source_name": source_name, "target_name": target_name}
+    expected = ArkivkopiRequest(arkivkopi_id, storage_account, container, sas_token, blob_info)
+    assert expected == testobj_arkivkopi_request_with_blob
 
 
 def test_arkivkopistatus_response_equals(arkivkopi_status_response):
