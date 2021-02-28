69b103fcee06ed98699a21f86efeeb49d191abc5
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-02-28 06:12:51 +0100

    MOL-298 Wait for clamd to open its port before starting the scanner script

--- s3-scan-tar/start.sh
+++ s3-scan-tar/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-echo Refreshing ClamAV signatures
-freshclam
-echo Starting ClamAV
-service clamav-daemon start
-echo Initializing scan...
-poetry run /opt/scanner.py
+echo "Refreshing ClamAV signatures"
+freshclam --quiet
+echo "Starting ClamAV"
+clamd &
+echo "Waiting for ClamAV to start"
+wait-for-it localhost:3310 --strict --timeout=30 -- python /app/scanner.py
