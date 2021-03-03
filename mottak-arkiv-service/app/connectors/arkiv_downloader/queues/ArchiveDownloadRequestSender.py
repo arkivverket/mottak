f5a7ffa6c5870b3661c1d627c6f827b854c1dbdb
Mårten Elmgren <elmgren@gmail.com>
2021-03-03 12:13:50 +0100

    MOL-326: use parameter class in ArchiveDownloadRequestSender

--- mottak-arkiv-service/app/connectors/arkiv_downloader/queues/ArchiveDownloadRequestSender.py
+++ mottak-arkiv-service/app/connectors/arkiv_downloader/queues/ArchiveDownloadRequestSender.py
@@ -1,6 +1,6 @@
 from app.connectors.arkiv_downloader.models import ArkivkopiRequest
 from app.connectors.azure_servicebus.azure_servicebus_client import AzureQueueSender
-from app.connectors.sas_generator.models import SASResponse
+from app.domain.models.Arkivkopi import ArkivkopiRequestParameters
 
 REQUEST_SENDER_QUEUE_NAME = 'archive-download-request'
 
@@ -13,25 +13,16 @@ class ArchiveDownloadRequestSender(AzureQueueSender):
     def __init__(self, connection_string: str):
         super().__init__(connection_string=connection_string, queue_name=REQUEST_SENDER_QUEUE_NAME)
 
-    async def send_archive_download_request(self, sas_token: SASResponse, arkivkopi_id: int) -> bool:
+    async def send_download_request(self, parameters: ArkivkopiRequestParameters) -> bool:
         """
-        Sends a archive download request on queue
-        :param sas_token: SAS token containing information about what to download, including access key
-        :param arkivkopi_id: id of this download request
+        Sends a download request on queue
+        :param parameters: a parameter class containing
+            - arkivkopi_id: id of this download request
+            - sas_token: SAS token containing information about what to download, including access key
+            - source_name: the name of the object to download from cloud storage if object download else None
+            - target_name: the name of the uploaded content on-prem, folder name or object name
         :return: True if message added to queue, else False
         """
-        arkivkopi_request = ArkivkopiRequest.for_archive_download(arkivkopi_id, sas_token)
+        arkivkopi_request = ArkivkopiRequest.from_parameters(parameters)
         return await super().send_message(arkivkopi_request.as_json_str())
 
-    async def send_object_download_request(self, sas_token: SASResponse, arkivkopi_id: int,
-                                           target_name: str, source_name: str) -> bool:
-        """
-        Sends a download request for an object on queue
-        :param sas_token: SAS token containing information about what to download, including access key
-        :param arkivkopi_id: id of this download request
-        :param target_name: the name of the object when uploaded to on-prem (a.k.a. filnavn in Arkivkopi)
-        :param source_name: the name of the object on cloud storage
-        :return: True if message added to queue, else False
-        """
-        arkivkopi_request = ArkivkopiRequest.for_object_download(arkivkopi_id, sas_token, target_name, source_name)
-        return await super().send_message(arkivkopi_request.as_json_str())
