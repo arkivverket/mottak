12334f4db5d2d0a9dd1aac3cb30cb633c533c278
Mårten Elmgren <elmgren@gmail.com>
2021-03-03 09:55:04 +0100

    MOL-326: update alembic migration for overforingspakke

--- mottak-arkiv-service/alembic/versions/bff48d665557_refactor_overforingspakke_table.py
+++ mottak-arkiv-service/alembic/versions/bff48d665557_refactor_overforingspakke_table.py
@@ -8,7 +8,6 @@ Create Date: 2021-03-03 08:12:52.476199
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = 'bff48d665557'
 down_revision = 'f2d569907a5c'
@@ -17,13 +16,32 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('overforingspakke', sa.Column('invitasjon_id', sa.Integer(), nullable=True))
-    op.execute('INSERT INTO overforingspakke (id, arkivuttrekk_id, invitasjon_id, tusd_id, tusd_objekt_navn, storrelse, status, opprettet, endret)  '
-               'SELECT o.id, o.arkivuttrekk_id, i.id, o.tusd_id, o.tusd_objekt_navn, o.storrelse, o.status, o.opprettet, o.endret '
-               'FROM invitasjon i, overforingspakke o '
-               'WHERE i.arkivuttrekk_id=o.arkivuttrekk_id')
+    # add invitasjon_id
+    op.add_column('overforingspakke', sa.Column('invitasjon_id', sa.Integer(), nullable=True, unique=True))
+    op.execute('UPDATE overforingspakke '
+               'SET invitasjon_id=( SELECT id '
+               '                    FROM invitasjon '
+               '                    WHERE overforingspakke.arkivuttrekk_id=invitasjon.arkivuttrekk_id '
+               '                    ORDER BY opprettet DESC limit 1)')
+    op.alter_column('overforingspakke', 'invitasjon_id', nullable=False)
+    op.create_foreign_key('overforingspakke_invitasjon_id_fkey', 'overforingspakke', 'invitasjon', ['invitasjon_id'], ['id'], )
 
-
+    # drop arkivuttrekk_id
+    op.drop_constraint('overforingspakke_arkivuttrekk_id_fkey', 'overforingspakke', )
+    op.drop_column('overforingspakke', 'arkivuttrekk_id')
 
 def downgrade():
+    # add arkivuttrekk_id
+    op.add_column('overforingspakke', sa.Column('arkivuttrekk_id', sa.Integer(), nullable=True, unique=True))
+    op.execute('UPDATE overforingspakke '
+               'SET arkivuttrekk_id=( SELECT arkivuttrekk_id '
+               '                      FROM invitasjon '
+               '                      WHERE overforingspakke.invitasjon_id=invitasjon.id '
+               '                      ORDER BY opprettet DESC limit 1)')
+    op.alter_column('overforingspakke', 'arkivuttrekk_id', nullable=False)
+    op.create_foreign_key('overforingspakke_arkivuttrekk_id_fkey', 'overforingspakke', 'arkivuttrekk', ['arkivuttrekk_id'],
+                          ['id'], )
+
+    # drop invitasjon_id
+    op.drop_constraint('overforingspakke_invitasjon_id_fkey', 'overforingspakke', )
     op.drop_column('overforingspakke', 'invitasjon_id')
