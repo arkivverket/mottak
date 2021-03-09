39c8308e91aeb608af229fce7676da77a4cefc23
Mårten Elmgren <elmgren@gmail.com>
2021-03-09 10:13:37 +0100

    MOL-326: PR fix, add domain model of Invitasjon. The only currently relevant fields in the domain is the id and ekstern_id from invitasjon

--- mottak-arkiv-service/app/domain/models/Invitasjon.py
+++ mottak-arkiv-service/app/domain/models/Invitasjon.py
@@ -1,6 +1,21 @@
 from enum import Enum
+from uuid import UUID
 
 
 class InvitasjonStatus(str, Enum):
     SENDT = 'Sendt'
     FEILET = "Feilet"
+
+
+class Invitasjon:
+    def __init__(self,
+                 id_: int,
+                 ekstern_id: UUID):
+        self.id = id_
+        self.ekstern_id = ekstern_id
+
+    def __eq__(self, other):
+        if isinstance(other, Invitasjon):
+            return self.id == other.id and \
+                   self.ekstern_id == other.ekstern_id
+        return False
