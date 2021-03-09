e8ac2bf4f3a5acf6922cb66cfa9164093797207f
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-03-09 14:21:29 +0100

    MOL-368 Fix the Snackbar box for alerts

--- mottak-arkiv-web/src/components/Alert.tsx
+++ mottak-arkiv-web/src/components/Alert.tsx
@@ -29,6 +29,13 @@ const useStyles = makeStyles((theme) => ({
 		display: 'flex',
 		alignItems: 'center',
 	},
+	snackbarRoot: {
+		display: 'flex',
+	},
+
+	snackbarMessage: {
+		flex: 1,
+	},
 }))
 
 /**
@@ -67,6 +74,10 @@ const Alert: React.FC<AlertContent> = ({ msg, type }): JSX.Element => {
 						<span className={classes.message}>{msg}</span>
 					</div>
 				}
+				classes={{
+					root: classes.snackbarRoot,
+					message: classes.snackbarMessage,
+				}}
 				action={[
 					<IconButton key="close" aria-label="Close" color="inherit" onClick={handleClose}>
 						<Icon>close</Icon>
