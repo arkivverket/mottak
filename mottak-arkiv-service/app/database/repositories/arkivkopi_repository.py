e1bf7390068223a1c0f86b42514c68a70d068818
Mårten Elmgren <elmgren@gmail.com>
2021-03-12 17:02:02 +0100

    MOL-328: Update query to use == instead of is because is returned empty lists

--- mottak-arkiv-service/app/database/repositories/arkivkopi_repository.py
+++ mottak-arkiv-service/app/database/repositories/arkivkopi_repository.py
@@ -1,9 +1,10 @@
+from typing import Optional
+
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
-from typing import Optional, List
 
 from app.database.dbo.mottak import Arkivkopi as Arkivkopi_DBO
 from app.domain.models.Arkivkopi import ArkivkopiStatus, Arkivkopi
-from sqlalchemy import desc
 
 
 def create(db: Session, arkivkopi: Arkivkopi) -> Arkivkopi_DBO:
@@ -20,7 +21,7 @@ def get_by_id(db: Session, id_: int) -> Arkivkopi_DBO:
 def get_archive_by_invitasjonId_newest(db: Session, invitasjon_id: int) -> Optional[Arkivkopi_DBO]:
     return db.query(Arkivkopi_DBO)\
         .filter(Arkivkopi_DBO.invitasjon_id == invitasjon_id)\
-        .filter(Arkivkopi_DBO.is_object is False)\
+        .filter(Arkivkopi_DBO.is_object == False)\
         .order_by(desc(Arkivkopi_DBO.endret))\
         .first()
 
@@ -28,7 +29,7 @@ def get_archive_by_invitasjonId_newest(db: Session, invitasjon_id: int) -> Optio
 def get_overforingspakke_by_invitasjonId_newest(db: Session, invitasjon_id: int) -> Optional[Arkivkopi_DBO]:
     return db.query(Arkivkopi_DBO)\
         .filter(Arkivkopi_DBO.invitasjon_id == invitasjon_id)\
-        .filter(Arkivkopi_DBO.is_object is True)\
+        .filter(Arkivkopi_DBO.is_object == True)\
         .order_by(desc(Arkivkopi_DBO.endret))\
         .first()
 
