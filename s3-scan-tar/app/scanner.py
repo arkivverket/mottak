6ce9c130e55e66dc0966a5f1a1c28cf566ae9d29
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-03-15 02:21:31 +0100

    MOL-366 Consistent use of `"`

--- s3-scan-tar/app/scanner.py
+++ s3-scan-tar/app/scanner.py
@@ -25,7 +25,7 @@ except ModuleNotFoundError:
     print("Failed to load dotenv file. Assuming production")
 
 MEGABYTES = 1024 ** 2
-UINT_MAX = 2**32-1
+UINT_MAX = 2 ** 32 - 1
 
 # Exit values
 UNKNOWNERROR = 1
@@ -35,19 +35,19 @@ CLAMAVERROR = 10
 
 
 def stream_tar(stream: Blob) -> tarfile.TarFile:
-    """ Takes a stream and created both a tarfile object
-    as well as a TarfileIterator using the stream """
+    """Takes a stream and created both a tarfile object
+    as well as a TarfileIterator using the stream"""
     try:
-        tar_file = tarfile.open(fileobj=stream, mode='r')
+        tar_file = tarfile.open(fileobj=stream, mode="r")
     except Exception as exception:
-        logging.critical(f'Failed to open stream to object {stream.client.container_name}/{stream.client.blob_name}')
-        logging.critical(f'Error: {exception}')
+        logging.critical(f"Failed to open stream to object {stream.client.container_name}/{stream.client.blob_name}")
+        logging.critical(f"Error: {exception}")
         raise exception
     return tar_file
 
 
 def scan_archive(blob: Blob, clamd_socket: ClamdUnixSocket, buffer_size: int) -> Tuple[int, int, int]:
-    """ Takes a tar_file typically a cloud storage object) and scans
+    """Takes a tar_file typically a cloud storage object) and scans
     it. Returns the named tuple (clean, virus, skipped)"""
     clean, virus, skipped = 0, 0, 0
     tar_file = stream_tar(blob)
@@ -58,7 +58,7 @@ def scan_archive(blob: Blob, clamd_socket: ClamdUnixSocket, buffer_size: int) ->
         if member.size > UINT_MAX:
             skipped += 1
             logging.warning(
-                f'Skipping {file_name} ({sizeof_fmt(member.size)}) because it exceeds the {sizeof_fmt(UINT_MAX)} size limit'
+                f"Skipping {file_name} ({sizeof_fmt(member.size)}) because it exceeds the {sizeof_fmt(UINT_MAX)} size limit"
             )
             continue
 
@@ -68,66 +68,65 @@ def scan_archive(blob: Blob, clamd_socket: ClamdUnixSocket, buffer_size: int) ->
             logging.debug(f"Handle ({file_name}) is none. Skipping...")
             continue
         try:
-            logging.info(f'Scanning {file_name} at {sizeof_fmt(member.size)}...')
+            logging.info(f"Scanning {file_name} at {sizeof_fmt(member.size)}...")
             result = clamd_socket.scan_stream(stream=tar_member, chunk_size=buffer_size)
 
             # No viruses found
             if result is None:
                 clean += 1
-                logging.info(f'clean - {file_name}')
+                logging.info(f"clean - {file_name}")
             else:
                 virus += 1
-                logging.critical(
-                    f'Virus found! {result["stream"][1]} in {file_name}')
+                logging.critical(f'Virus found! {result["stream"][1]} in {file_name}')
 
         except ConnectionResetError:
             skipped += 1
-            logging.error(
-                'clamd reset the connection. Increase max scan size for clamd.')
-            logging.warning(f'SKIPPED (File too big) - {file_name}')
+            logging.error("clamd reset the connection. Increase max scan size for clamd.")
+            logging.warning(f"SKIPPED (File too big) - {file_name}")
             continue
         except Exception as exception:
             logging.error(f"Failed to scan {file_name}")
-            logging.error(f'Error: {exception}')
+            logging.error(f"Error: {exception}")
             raise exception
 
-    logging.debug(f'clean: {clean}, virus: {virus}, skipped: {skipped}')
+    logging.debug(f"clean: {clean}, virus: {virus}, skipped: {skipped}")
     ret = namedtuple("scan", ["clean", "virus", "skipped"])
     return ret(clean, virus, skipped)
 
 
 def main():
     """ Run from here, really """
-    logging.basicConfig(level=logging.INFO, filename='/tmp/avlog', filemode='w',
-                        format='%(asctime)s | %(levelname)s | %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, filename="/tmp/avlog", filemode="w", format="%(asctime)s | %(levelname)s | %(message)s"
+    )
     logging.getLogger().addHandler(logging.StreamHandler())
-    logging.getLogger('azure.core.pipeline.policies.http_logging_policy').setLevel(logging.ERROR)
+    logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(logging.ERROR)
     logging.info("Starting s3-scan-tar")
 
     # Also known as a Contaienr in Azure Blob Storage
-    bucket = os.getenv('BUCKET')
-    objectname = os.getenv('TUSD_OBJECT_NAME')
+    bucket = os.getenv("BUCKET")
+    objectname = os.getenv("TUSD_OBJECT_NAME")
     buffer_size = int(os.getenv("BUFFER_SIZE", DEFAULT_BUFFER_SIZE))
     max_concurrency = int(os.getenv("MAX_CONCURRENCY", 32))
 
     # Test the access to the object stream, so we can return early if there are any issues
-    logging.info('Initialising connection to Azure Blob Storage')
+    logging.info("Initialising connection to Azure Blob Storage")
     try:
         blob = Blob(
             BlobClient.from_connection_string(
                 conn_str=os.getenv("AZURE_STORAGE_CONNECTION_STRING"),
                 container_name=bucket,
-                blob_name=objectname
+                blob_name=objectname,
             ),
             max_concurrency,
             buffer_size,
         )
-        logging.info(f'Connected to Azure with API version {blob.client.api_version}')
+        logging.info(f"Connected to Azure with API version {blob.client.api_version}")
     except ResourceNotFoundError:
-        logging.critical(f'An error occured while getting the object handle {objectname} in bucket {bucket}')
+        logging.critical(f"An error occured while getting the object handle {objectname} in bucket {bucket}")
         sys.exit(OBJECTERROR)
     except ClientAuthenticationError:
-        logging.critical('An error occured while connecting to Azure, please check the credentials')
+        logging.critical("An error occured while connecting to Azure, please check the credentials")
         sys.exit(AZUREERROR)
     except Exception as exception:
         logging.critical("Unkown error occured while getting the object")
@@ -142,11 +141,11 @@ def main():
     logging.info("Starting ClamAV")
     os.system("clamd &")
 
-    logging.info('Initialising connection to clamd')
+    logging.info("Initialising connection to clamd")
     try:
         wait_for_port(3310)
         clamd_socket = ClamdUnixSocket()
-        logging.info(f'Connected to clamd version {clamd_socket.version()}')
+        logging.info(f"Connected to clamd version {clamd_socket.version()}")
     except TimeoutError as exception:
         logging.critical(exception)
         sys.exit(CLAMAVERROR)
@@ -159,8 +158,8 @@ def main():
         logging.critical(exception)
         sys.exit(CLAMAVERROR)
 
-    logging.info(f'Initialising scan on {bucket}/{objectname} with a scan limit of {sizeof_fmt(UINT_MAX)}')
-    logging.info(f'Buffer size is set to {sizeof_fmt(buffer_size)}')
+    logging.info(f"Initialising scan on {bucket}/{objectname} with a scan limit of {sizeof_fmt(UINT_MAX)}")
+    logging.info(f"Buffer size is set to {sizeof_fmt(buffer_size)}")
     scan_ret = scan_archive(blob, clamd_socket, buffer_size)
 
     logging.info(f"{scan_ret.clean} files scanned and found clean")
