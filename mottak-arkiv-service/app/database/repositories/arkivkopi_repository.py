7373a8b3a47f50657c54e0baf5e44239603c3c51
Mårten Elmgren <elmgren@gmail.com>
2021-03-15 12:35:19 +0100

    MOL-328: PR feedback. Consolidate repository methods for retrival of either archive or overforingspakke into one method

--- mottak-arkiv-service/app/database/repositories/arkivkopi_repository.py
+++ mottak-arkiv-service/app/database/repositories/arkivkopi_repository.py
@@ -18,19 +18,13 @@ def get_by_id(db: Session, id_: int) -> Arkivkopi_DBO:
     return db.query(Arkivkopi_DBO).get(id_)
 
 
-def get_archive_by_invitasjonId_newest(db: Session, invitasjon_id: int) -> Optional[Arkivkopi_DBO]:
-    return db.query(Arkivkopi_DBO)\
-        .filter(Arkivkopi_DBO.invitasjon_id == invitasjon_id)\
-        .filter(Arkivkopi_DBO.is_object == False)\
-        .order_by(desc(Arkivkopi_DBO.endret))\
-        .first()
-
-
-def get_overforingspakke_by_invitasjonId_newest(db: Session, invitasjon_id: int) -> Optional[Arkivkopi_DBO]:
-    return db.query(Arkivkopi_DBO)\
-        .filter(Arkivkopi_DBO.invitasjon_id == invitasjon_id)\
-        .filter(Arkivkopi_DBO.is_object == True)\
-        .order_by(desc(Arkivkopi_DBO.endret))\
+def get_by_invitasjon_id_and_is_object_newest(db: Session,
+                                              invitasjon_id: int,
+                                              is_object: bool) -> Optional[Arkivkopi_DBO]:
+    return db.query(Arkivkopi_DBO) \
+        .filter(Arkivkopi_DBO.invitasjon_id == invitasjon_id) \
+        .filter(Arkivkopi_DBO.is_object == is_object) \
+        .order_by(desc(Arkivkopi_DBO.endret)) \
         .first()
 
 
